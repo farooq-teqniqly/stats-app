18cbbcd087c0bb335c34362ef4e2ddd11a981257
Farooq Mahmud <mahmud.farooq@acuitybrands.com>
2024-02-10 21:48:05 -0800

    wip

--- src/ttest/ttest.test.ts
+++ src/ttest/ttest.test.ts
@@ -25,4 +25,17 @@ describe("Independent samples t-test using descriptive statistics", () => {
     expect(testResult.groups[sampleSummmary1.name].error).toBe(0.8);
     expect(testResult.groups[sampleSummmary2.name].error).toBe(0.6);
   });
+
+  it("has the groups' degrees of freedom", () => {
+    expect(testResult.groups[sampleSummmary1.name].df).toBe(24);
+    expect(testResult.groups[sampleSummmary2.name].df).toBe(24);
+  });
+
+  it("has the standard error of the difference between means", () => {
+    expect(testResult.error).toBe(1);
+  });
+
+  it("has the total degrees of freedom", () => {
+    expect(testResult.df).toBe(48);
+  });
 });
--- src/ttest/ttest.ts
+++ src/ttest/ttest.ts
@@ -9,7 +9,9 @@ type TTestResult = {
   t: {
     observed: number;
   };
-  groups: Record<string, { error: number }>;
+  groups: Record<string, { error: number; df: number }>;
+  error: number;
+  df: number;
 };
 
 const independentSamplesTTest = (
@@ -23,11 +25,15 @@ const independentSamplesTTest = (
     groups: {
       [sampleSummary1.name]: {
         error: 0.8,
+        df: 24,
       },
       [sampleSummary2.name]: {
         error: 0.6,
+        df: 24,
       },
     },
+    error: 1,
+    df: 48,
   };
 };
 
